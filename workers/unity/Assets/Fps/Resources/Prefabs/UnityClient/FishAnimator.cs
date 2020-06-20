a4654395e7f5dbd4ccea31e4bebd241df1451d95
LinsIan <jjes20055114123@gmail.com>
2020-06-20 12:13:44 +0800

    魚Client端邏輯製作

--- workers/unity/Assets/Fps/Resources/Prefabs/UnityClient/FishAnimator.cs
+++ workers/unity/Assets/Fps/Resources/Prefabs/UnityClient/FishAnimator.cs
@@ -8,10 +8,26 @@ namespace Fps.Animation
 {
     public class FishAnimator : MonoBehaviour
     {
-        [SerializeField] private Animator m_Animator;
         private const string SWIM_ANIMATION = "Swim";
         private const string DEAD_ANIMATION = "Dead";
+        private Animator m_Animator;
 
+        private void OnEnable()
+        {
+            m_Animator = GetComponent<Animator>();
+        }
 
+        public void PlayAnimation(EFishState eState)
+        {
+            switch(eState)
+            {
+                case EFishState.SWIM:
+                    m_Animator.Play(SWIM_ANIMATION);
+                    break;
+                case EFishState.DEAD:
+                    m_Animator.Play(DEAD_ANIMATION);
+                    break;
+            }
+        }
     }
 }
--- workers/unity/Assets/Fps/Resources/Prefabs/UnityClient/FishClientDriver.cs
+++ workers/unity/Assets/Fps/Resources/Prefabs/UnityClient/FishClientDriver.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using Improbable.Gdk.Subscriptions;
 using Fps.Config;
+using Fps.SchemaExtensions;
+using Improbable;
 
 namespace Fps
 {
@@ -15,19 +17,43 @@ namespace Fps
     [WorkerType(WorkerUtils.UnityClient)]
     public class FishClientDriver : MonoBehaviour
     {
+        [Require] private PositionReader position;
         [Require] private ClientRotationWriter rotation;
         [Require] private HealthComponentReader health;
-        // Start is called before the first frame update
-        void Start()
-        {
 
+        private Animation.FishAnimator animator;
+
+        private EFishState eState;
+
+        private void OnEnable()
+        {
+            animator = GetComponent<Animation.FishAnimator>();
+            position.OnUpdate += OnPositionUpdate;
+            rotation.OnUpdate += OnRotationUpdate;
+            health.OnUpdate += OnHealthComponentUpdate;
+            eState = EFishState.SWIM;
         }
 
-        // Update is called once per frame
-        void Update()
+        private void OnPositionUpdate(Position.Update update)
         {
+            transform.position = position.Data.Coords.ToUnityVector();
+        }
 
+        private void OnRotationUpdate(ClientRotation.Update update)
+        {
+            float pitch = rotation.Data.Latest.Pitch.ToFloat1k();
+            float roll = rotation.Data.Latest.Roll.ToFloat1k();
+            float paw = rotation.Data.Latest.Yaw.ToFloat1k();
+            transform.rotation = Quaternion.Euler(pitch, roll, paw);
+        }
+
+        private void OnHealthComponentUpdate(HealthComponent.Update update)
+        {
+            if(health.Data.Health <= 0)
+            {
+                eState = EFishState.DEAD;
+                animator.PlayAnimation(eState);
+            }
         }
     }
 }
-
