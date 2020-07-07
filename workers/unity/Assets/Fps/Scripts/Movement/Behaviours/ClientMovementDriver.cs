82cb07031e86cdba17ec0259bb18619b2e627fcc
LinsIan <jjes20055114123@gmail.com>
2020-07-07 14:12:05 +0800

    位置校正增加距離檢查

--- workers/unity/Assets/Fps/Scripts/Movement/Behaviours/ClientMovementDriver.cs
+++ workers/unity/Assets/Fps/Scripts/Movement/Behaviours/ClientMovementDriver.cs
@@ -168,7 +168,12 @@ namespace Fps.Movement
 
         private void OnServerUpdate(ServerResponse update)
         {
-            //Reconcile(update.Position.ToVector3() + origin, update.Timestamp);
+            //設定誤差範圍，差太多則進行位置校正，避免作弊
+            if(Vector3.Distance(update.Position.ToVector3() + origin, transform.position) >= 10f)
+            {
+                Reconcile(update.Position.ToVector3() + origin, update.Timestamp);
+            }
+            
         }
 
         public void ApplyMovement(Vector3 movement, Quaternion rotation, MovementSpeed movementSpeed, bool startJump)
